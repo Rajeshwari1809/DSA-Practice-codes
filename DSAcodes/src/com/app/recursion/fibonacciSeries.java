ea64ce26b4957fa23d8d0a1e172cb67bc9d0005c
rajeshwari <rajeshwarijadhav1809@gmail.com>
2025-01-16 23:56:11 +0530

    day4

--- DSAcodes/src/com/app/recursion/fibonacciSeries.java
+++ DSAcodes/src/com/app/recursion/fibonacciSeries.java
@@ -1,9 +1,36 @@
 package com.app.recursion;
 
 public class fibonacciSeries {
+	
+	
+
+	
+	
+	public static void printfibonacci(int n, int a, int b)
+	{
+		
+		if(n==0)
+		{
+			return ;
+			
+		}
+		
+		int c=a+b;
+		System.out.println(c);
+		a=b;
+		printfibonacci(b,c,n-1);
+		
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int a=0,b=1;
+		System.out.print(a);
+		System.out.println(b);
+		int n =7;
+		printfibonacci(a, b, n-2);
+		
+		
+		
 
 	}
 
